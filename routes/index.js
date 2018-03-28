4990f5378fcc178f402aa68c6c34947b32ceae3c
Will <0920076@utp.edu.pe>
2018-03-28 01:21:27 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+const IndexController = require('../controllers/index');
+
+router.get('/', IndexController.index);
+router.get('/products', IndexController.ListOfProducts); 
+
+router.post('/new-product', IndexController.newProduct)
+
+module.exports = router;
+
